064ec8e942088f821ceab9a58aabf6ae70c04bf2
sevenkinako <sevenkinako.0609@gmail.com>
2025-09-21 23:59:20 +0900

    JavaScript5章課題再々提出

---
+++ kadai_005/bmi.js
@@ -0,0 +1,14 @@
+// 変数の宣言
+let weight;
+let height;
+let bmi;
+
+// 値の代入
+weight = ( 68 );
+height = ( 1.7 );
+bmi = ( 68 ) / ( 1.7 * 1.7 );
+
+//コンソールへの出力
+console.log(weight);
+console.log(height);
+console.log(bmi);
